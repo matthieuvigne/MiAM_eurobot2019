67685b396e067ce9e285ef992d8d8c2445631e66
14chanwa <quentin.cwn@gmail.com>
2019-01-16 23:46:31 +0100

    Reorganise project and make a basic structure for the library

---
+++ MPC/PathPlanner/src/miam_pp_base.cpp
@@ -0,0 +1,16 @@
+#include <miam_pp_base.hpp>
+#include <iostream>
+
+using namespace miam_pp;
+
+Trajectory miam_pp::get_planned_trajectory_main_robot(
+    State current_state,
+    WayPointList waypoint_list,
+    float planning_horizon
+) {
+    // TODO
+    std::cout << "get_planned_trajectory_main_robot" << std::endl;
+    return Trajectory();
+}
+
+
