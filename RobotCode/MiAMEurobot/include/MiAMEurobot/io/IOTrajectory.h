9ea9164151f5e66b2315fbc07b26b3cd87522824
14chanwa <spam-cwn@hotmail.com>
2019-02-20 23:29:57 +0100

    [MiAMEurobot/io] Add io functions to read and write SampleTrajectory

---
+++ RobotCode/MiAMEurobot/include/MiAMEurobot/io/IOTrajectory.h
@@ -0,0 +1,19 @@
+/// \file io/IOTrajectory.h
+/// \brief IO functions for trajectories.
+#ifndef MIAM_IO_IOTrajectory
+#define MIAM_IO_IOTrajectory
+
+	#include "MiAMEurobot/trajectory/SampledTrajectory.h"
+	#include <string>
+	#include <iostream>
+
+	namespace miam{
+		namespace io{
+			
+			trajectory::SampledTrajectory readSampledTrajectoryFromFile(std::string file_name);
+			void writeSampledTrajectoryToFile(trajectory::SampledTrajectory trajectory, std::string file_name);
+	
+		}
+	}
+
+#endif
