35122d25c1807d1ff286ee21819200c1685d6e86
14chanwa <spam-cwn@hotmail.com>
2019-02-21 00:31:30 +0100

    [PathPlanner] Update functions and add new tests
    * Use fixed start and end TrajectoryPoint
    * Generate trajectories (straight trajectory has an issue)

--- PathPlanner/tests/basic_test.cpp
+++ PathPlanner/tests/basic_test.cpp
@@ -41,11 +41,25 @@ int main( ){
     waypoint_list.push_back(miam::RobotPosition(800.0, 125.0, 0.5));
     waypoint_list.push_back(miam::RobotPosition(1000.0, 200.0, 1.0));
 
+    miam::trajectory::TrajectoryPoint first_trajectory_point;
+    first_trajectory_point.position = waypoint_list.front();
+    first_trajectory_point.linearVelocity = 0.0;
+    first_trajectory_point.angularVelocity = 0.0;
+    miam::trajectory::TrajectoryPoint last_trajectory_point;
+    last_trajectory_point.position = waypoint_list.back();
+    last_trajectory_point.linearVelocity = 0.0;
+    last_trajectory_point.angularVelocity = 0.0;
+    
+    /* 
+     * Trajectory planning 
+     */
     miam::trajectory::SampledTrajectory sampled_trajectory = miam_pp::get_planned_trajectory_main_robot(
         waypoint_list,
+        first_trajectory_point,
+        last_trajectory_point,
         true, // Plot output
         true  // Print output
-    );
+	);
     
     int N = std::floor(sampled_trajectory.getDuration() / 0.01);
     double tstep = sampled_trajectory.getDuration() / N;
