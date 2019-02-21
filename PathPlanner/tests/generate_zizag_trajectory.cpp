8b0c0b0701ef1e1d6f9a144e7a96db11f9deefd8
14chanwa <spam-cwn@hotmail.com>
2019-02-21 23:45:29 +0100

    [PathPlanner] Fix issue in which one could not solve more than 1 OCP
    * ACADO uses static counters for diff states, controls and params
    These counters have to be reset before each OCP
    
    * Add a zigzag curve example

--- PathPlanner/tests/generate_zizag_trajectory.cpp
+++ PathPlanner/tests/generate_zizag_trajectory.cpp
@@ -16,6 +16,9 @@ int main( ){
     
     cout << "Computing trajectory " << trajectory_name << endl;
     
+    
+    //// First half
+    
     miam_pp::WayPointList waypoint_list = miam_pp::WayPointList();
     waypoint_list.push_back(miam::RobotPosition(0.0, 0.0, 0.0));
     waypoint_list.push_back(miam::RobotPosition(500.0, 100.0, 0.5));
@@ -28,7 +31,7 @@ int main( ){
     first_trajectory_point.angularVelocity = 0.0;
     miam::trajectory::TrajectoryPoint last_trajectory_point;
     last_trajectory_point.position = waypoint_list.back();
-    last_trajectory_point.linearVelocity = 100.0;
+    last_trajectory_point.linearVelocity = 200.0;
     last_trajectory_point.angularVelocity = 0.0;
     
     /* 
@@ -40,9 +43,54 @@ int main( ){
         last_trajectory_point,
         true, // Plot output
         true  // Print output
-);
+    );
     
-    miam::io::writeSampledTrajectoryToFile(sampled_trajectory, trajectory_name);
+    //// Second half
+    
+    miam_pp::WayPointList waypoint_list_2 = miam_pp::WayPointList();
+    waypoint_list_2.push_back(miam::RobotPosition(1000.0, 200.0, 1.0));
+    waypoint_list_2.push_back(miam::RobotPosition(1200.0, 275.0, 0.5));
+    waypoint_list_2.push_back(miam::RobotPosition(1500.0, 300.0, 0.5));
+    waypoint_list_2.push_back(miam::RobotPosition(2000.0, 400.0, 0.0));
+    
+    miam::trajectory::TrajectoryPoint first_trajectory_point_2;
+    first_trajectory_point_2.position = waypoint_list_2.front();
+    first_trajectory_point_2.linearVelocity = 200.0;
+    first_trajectory_point_2.angularVelocity = 0.0;
+    miam::trajectory::TrajectoryPoint last_trajectory_point_2;
+    last_trajectory_point_2.position = waypoint_list_2.back();
+    last_trajectory_point_2.linearVelocity = 0.0;
+    last_trajectory_point_2.angularVelocity = 0.0;
+    
+     /* 
+     * Trajectory planning 
+     */
+    miam::trajectory::SampledTrajectory sampled_trajectory_2 = miam_pp::get_planned_trajectory_main_robot(
+        waypoint_list_2,
+        first_trajectory_point_2,
+        last_trajectory_point_2,
+        true, // Plot output
+        true  // Print output
+    );
+    
+    //// Combine trajectories
+    miam_pp::TrajectoryVector output_trajectory_vector = sampled_trajectory.getUnderlyingPoints();
+    // Remove last point (duplicate)
+    output_trajectory_vector.pop_back();
+    // Concatenate with second half
+    miam_pp::TrajectoryVector output_trajectory_vector_2 = sampled_trajectory_2.getUnderlyingPoints();
+    output_trajectory_vector.insert(
+        output_trajectory_vector.end(),
+        output_trajectory_vector_2.begin(),
+        output_trajectory_vector_2.end()
+    );
+    
+    miam::trajectory::SampledTrajectory sampled_trajectory_final(
+        output_trajectory_vector,
+        sampled_trajectory.getDuration() + sampled_trajectory_2.getDuration()
+    );
+    
+    miam::io::writeSampledTrajectoryToFile(sampled_trajectory_final, trajectory_name);
 
 	return 0;
 }
