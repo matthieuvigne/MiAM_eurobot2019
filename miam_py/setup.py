eae9dfd31adf2448da224aea21072a686620c080
Matthieu Vigne <matthieu.vigne@laposte.net>
2019-02-24 17:25:33 +0100

    [miam_py] Bugfixes and enhancements.
     - Fix miam_plot crash when asking to draw odd numbers of plots.
     - Add grid to miam_plot axes.
     - Improve layout of miam_plot to enhance readability.
     - Handle aspect ratio in miam_analyse_tracking.
     - Switch whole project from tabs to spaces.

--- miam_py/setup.py
+++ miam_py/setup.py
@@ -6,12 +6,12 @@ from setuptools import setup
 
 setup(
     name='miam_py',
-    version='0.1.0',
+    version='0.1.1',
     description='MiAM robotics log processing',
     long_description=open('README.md').read(),
     packages=['miam_py'],
     package_dir={'': 'src'},
     scripts=["scripts/miam_plot",
              "scripts/miam_analyse_tracking",],
-	data_files=[('images', ['images/table.png'])],
+    data_files=[('images', ['images/table.png'])],
     zip_safe=False)
