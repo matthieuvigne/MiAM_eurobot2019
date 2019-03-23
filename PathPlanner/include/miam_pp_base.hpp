3cbddd6b96e0048e331931471a28f0ed659683ad
14chanwa <spam-cwn@hotmail.com>
2019-03-23 17:57:23 +0100

    [MainRobotCode/MPC][PathPlanner] Fix ocp & add PathPlanner init by traj
    * OCP was not defined properly (lacked one constraint)
    
    * Fix N in PP OCP for consistency (number of intervals)
    
    * Now PP can be initialized with a Trajectory

--- PathPlanner/include/miam_pp_base.hpp
+++ PathPlanner/include/miam_pp_base.hpp
@@ -14,35 +14,53 @@
 #include <MiAMEurobot/trajectory/Utilities.h>
 
 
-
+using namespace miam;
+using namespace miam::trajectory;
 
 
 namespace miam_pp {
 
-/**
- * A list of positions: ((x, y), (x, y)...).
- */
-typedef std::vector<miam::RobotPosition > WayPointList;
+    /**
+     * A list of positions: ((x, y), (x, y)...).
+     */
+    typedef std::vector<RobotPosition > WayPointList;
 
-/**
- * A trajectory: ((t, state, control), (t, state, control)...).
- */
-typedef std::vector<miam::trajectory::TrajectoryPoint > TrajectoryVector;
+    /**
+     * A trajectory: ((t, state, control), (t, state, control)...).
+     */
+    typedef std::vector<::TrajectoryPoint > TrajectoryVector;
 
 
-/**
- * Given a current state, a list of waypoints and a planning horizon,
- * compute a suitable trajectory. Assume the dynamics are those of the
- * main robot.
- */
-miam::trajectory::SampledTrajectory get_planned_trajectory_main_robot(
-    WayPointList waypoint_list,
-    miam::trajectory::TrajectoryPoint first_trajectory_point,
-    miam::trajectory::TrajectoryPoint last_trajectory_point,
-    bool plot = false,
-    bool verbose = false,
-    bool vlin_free_at_end = false
-);
+    SampledTrajectory get_resampled_trajectory_main_robot(
+        WayPointList waypoint_list,
+        TrajectoryPoint first_trajectory_point,
+        TrajectoryPoint last_trajectory_point
+    );
+
+    SampledTrajectory get_planned_trajectory_main_robot(
+        WayPointList waypoint_list,
+        TrajectoryPoint first_trajectory_point,
+        TrajectoryPoint last_trajectory_point,
+        bool plot = false,
+        bool verbose = false,
+        bool vlin_free_at_end = false
+    );
+
+
+    /**
+     * Given a current state, a list of waypoints and a planning horizon,
+     * compute a suitable trajectory. Assume the dynamics are those of the
+     * main robot.
+     */
+    SampledTrajectory get_planned_trajectory_main_robot_from_init(
+        Trajectory& init_trajectory,
+        TrajectoryPoint first_trajectory_point,
+        TrajectoryPoint last_trajectory_point,
+        bool plot = false,
+        bool verbose = false,
+        bool vlin_free_at_end = false,
+        int N = -1
+    );
 
 }
 
